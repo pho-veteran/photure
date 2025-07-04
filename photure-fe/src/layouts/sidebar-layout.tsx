8ee833173ea778758963f98c791cb60dac258931
pho-veteran <thanhvinhlqt2016@gmail.com>
2025-07-04 19:38:48 +0700

    feats: sidebar toggle

--- photure-fe/src/layouts/sidebar-layout.tsx
+++ photure-fe/src/layouts/sidebar-layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
 
 interface SidebarProps {
   children?: React.ReactNode;
@@ -9,6 +10,15 @@ const SidebarLayout: React.FC<SidebarProps> = ({ children }) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        <header className="flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </div>
+        </header>
         {children}
       </SidebarInset>
     </SidebarProvider>
